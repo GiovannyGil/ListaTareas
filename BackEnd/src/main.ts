5124401cf3daf97f1e728955dae3b6eac228538c
Giovanny <102708414+GiovannyGil@users.noreply.github.com>
2025-07-22 19:58:26 -0500

    quitar el "HttpOnly Cookie"

--- BackEnd/src/main.ts
+++ BackEnd/src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
   app.enableCors({
     origin: 'http://localhost:4200', // URL de tu frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
